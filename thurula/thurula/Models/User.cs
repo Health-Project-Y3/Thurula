e8c015458a9547e48a26e11f14b04c224c080505
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-06-28 01:36:43 +0530

    Created User Model and Service

---
+++ thurula/thurula/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace thurula.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+    [Required]
+    [MaxLength(30)]
+    [BsonElement("fname")]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [MaxLength(30)]
+    [BsonElement("lname")]
+    public string LastName { get; set; } = string.Empty;
+
+    
+    
+}
