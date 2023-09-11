9fc0b21767660053647dfac7216319238d3ef4df
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-11 22:06:09 +0530

    Added PatchDoc to UserController

--- thurula/thurula/Models/UserDto.cs
+++ thurula/thurula/Models/UserDto.cs
@@ -4,7 +4,7 @@ public class UserDto
 {
     public required string Username { get; set; }
     public required string Password { get; set; }
-    
+    public required string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
 }
