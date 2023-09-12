784bdaff48003df913a0d916cd40d537a82a6600
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-12 21:02:58 +0530

    User Exercise Records, Models, Services and Controller

---
+++ thurula/thurula/Services/IUserExerciseService.cs
@@ -0,0 +1,13 @@
+﻿using thurula.Models;
+
+namespace thurula.Services;
+
+public interface IUserExerciseService
+{
+    List<UserExercise> Get();
+    UserExercise Get(string id);
+    UserExercise Create(UserExercise uex);
+    List<UserExercise> GetByUser(string userId, DateTime? start, DateTime? end);
+    void Update(string id, UserExercise uex);
+    void Remove(string id);
+}
