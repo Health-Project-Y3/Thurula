04c37f6ea6c757e03f32c3c123c80d934ba0d9b9
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-12 12:44:03 +0530

    Added gender

--- thurula/thurula/Controllers/BabyController.cs
+++ thurula/thurula/Controllers/BabyController.cs
@@ -52,9 +52,9 @@ public class BabyController : ControllerBase
         {
             _babyService.Create(baby);
             return CreatedAtRoute("GetBaby", new { id = baby.Id }, baby);
-        } catch (Exception)
+        } catch (Exception e)
         {
-            return BadRequest();
+            return BadRequest(e);
         }
     }
 
