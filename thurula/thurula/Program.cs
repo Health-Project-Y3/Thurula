06d396addca35b82faf9512c467d4bfa4baeb271
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-10-30 23:39:04 +0530

    Add dependency injection

--- thurula/thurula/Program.cs
+++ thurula/thurula/Program.cs
@@ -69,6 +69,7 @@ builder.Services.AddScoped<IUserExerciseService, UserExerciseService>();
 builder.Services.AddScoped<IUserWeightService, UserWeightService>();
 builder.Services.AddScoped<IUserDrinkingService, UserDrinkingService>();
 builder.Services.AddScoped<IUserBpService, UserBpService>();
+builder.Services.AddScoped<IBmiService, BmiService>();
 
 
 builder.Services.AddSignalR(hubOptions => {
